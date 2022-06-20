2fb9a1dfd9d3e2258ffc66ec722a3247b9e4879e
David Bayley <davidbayley80@hotmail.com>
2022-06-19 20:40:36 -0600

    Refined Unit Tests

---
+++ TradeServiceLibary/PortfolioAggregator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TradeService
+{
+    public class PortfolioAggregator
+    {
+
+        public PortfolioAggregator() { }
+
+        // Link method for 
+        public double GetTotalPrice(IEnumerable<TradeAttributes> tradeItems) => tradeItems.Sum(t => t._price);
+
+        // Async method for Trade Aggregation
+        public async Task<double> GetTotalPriceAsync(BlockingCollection<TradeAttributes> tradeItems)
+        {
+
+            return await Task.Run(() =>
+            {
+                double totalPrice = 0;
+                foreach (var tradeRecord in tradeItems.GetConsumingEnumerable())
+                {
+                    totalPrice += tradeRecord._price;
+                }
+
+                return Task.FromResult(totalPrice);
+            });
+        }
+    }
+}
+
---
+++ TradeServiceTests/TradeServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using TradeService;
+
+namespace TradeServiceTests;
+
+public class TradeService_Test
+{
+
+    public TradeQueueLoader? tradeLoader;
+    public PortfolioAggregator? portfolioAggregator;
+
+    [SetUp]
+    public void Setup()
+    {
+        tradeLoader = new TradeQueueLoader("TradeDetails.csv");
+        portfolioAggregator = new PortfolioAggregator();
+    }
+
+    [Test]
+    public async Task Test_CSVImport()
+    {
+        // Checks time-perofrmance and asserts data
+        var customer = "Barclays";
+        int timeout = 10000;
+        var task = tradeLoader.LoadAsync();
+        if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+        {
+            Assert.That(task.Status == TaskStatus.RanToCompletion);
+            Assert.That(tradeLoader.TradeRecords.Count > 0);
+            string firstCustomerResult = tradeLoader.TradeRecords.FirstOrDefault()._customer;
+            Assert.AreEqual(customer, firstCustomerResult);
+        }
+        else
+        {
+            throw new TimeoutException("Time Limit for CSV Import has been exceeded, check input data");
+        }
+    }
+
+
+    [Test]
+    public async Task Test_PortfolioAggregator()
+    { 
+        var priceReader = portfolioAggregator.GetTotalPriceAsync(tradeLoader.TradeRecords);
+        await Task.WhenAll(tradeLoader.LoadAsync(), priceReader);
+        Assert.That(priceReader.Result, Is.EqualTo(8975857));
+
+    }
+}
