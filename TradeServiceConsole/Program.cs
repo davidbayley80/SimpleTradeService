5696fb406f3400b510521d70aa8e1313338f4d01
David Bayley <davidbayley80@hotmail.com>
2022-06-19 21:07:24 -0600

    Comment update and Library name change

--- TradeServiceConsole/Program.cs
+++ TradeServiceConsole/Program.cs
@@ -9,7 +9,7 @@ class Program
 
             var priceReader = portfolioAggregator.GetTotalPriceAsync(tradeLoader.TradeRecords);
 
-            // I've loaded in the classes now i need to kick the load off via async
+            // kick the load off via async
             await Task.WhenAll(tradeLoader.LoadAsync(), priceReader);
 
             Console.WriteLine($"Total Valuation:{priceReader.Result}");
